f318871df3c27f8d63791680a093416307bf3335
LittleRey <34397448+LittleRey@users.noreply.github.com>
2021-02-01 21:58:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,3 @@
-C_VER="v1.0.3"
-mkdir /caddybin
-cd /caddybin
-CADDY_URL="https://github.com/caddyserver/caddy/releases/download/$C_VER/caddy_${C_VER}_linux_amd64.tar.gz"
-echo ${CADDY_URL}
-wget --no-check-certificate -qO 'caddy.tar.gz' ${CADDY_URL}
-tar xvf caddy.tar.gz
-rm -rf caddy.tar.gz
-chmod +x caddy
-
-
-cat <<-EOF > /caddybin/Caddyfile
-http://0.0.0.0:${PORT}
-{
-	proxy /package localhost:8080
-}
-EOF
 
 cd /usr/src/app
-node app.js -p 8080 -o migu &
-cd /caddybin
-./caddy -conf="Caddyfile"
+node app.js -p 8080 -o migu
